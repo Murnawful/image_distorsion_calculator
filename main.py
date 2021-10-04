49af972c0c12f0e0312a9e39cc5851c93fe47198
NF <nathan.farbersound@gmail.com>
2021-10-04 16:19:46 +0200

    Backup

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from irm_dist_calculator import imageGrid as im
 # (0.109, 0.1095, 0.074)  # 3DT2_coreg
 # (0.134, 0.134, 0.184)  # Protons_fiduc
 # (0.105, 0.105, 0.205)  # T1Spir_30slices_coreg
+# (0.105, 0.105, 0.205)  # T1Spir_30slices_FOV_coreg
 grid = ref.ReferenceGrid(center=(0.105, 0.105, 0.205))
 grid.build()
 grid.convert()
@@ -26,6 +27,7 @@ ref_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.05, origin=
 # roi = ((100, 85, 50), (160, 170, 130))  # 3DT1_160slices_fiduc
 # roi = ((100, 100, 0), (312, 412, 49))  # Protons_fiduc
 # roi = ((281, 213, 0), (505, 545, 29))  # T1Spir_30slices_coreg
+# roi = ((306, 235, 0), (560, 598, 29))  # T1Spir_30slices_FOV_coreg
 roi = ((306, 235, 0), (560, 598, 29))
 source = im.ImageGrid("../../im_DICOM/complete_IRM_CBCT/" + name + "/", 115, range_hu=(150, 200), is_mri=True,
                       image_roi=roi)
